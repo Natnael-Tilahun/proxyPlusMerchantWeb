511a24a7b26f70a8d25d68a21c40e359374346c9
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2025-08-08 19:49:26 +0300

    Modify endpoints

--- composables/useOperators.ts
+++ composables/useOperators.ts
@@ -196,7 +196,7 @@ export const useOperators = () => {
   const getMerchantOperatorRoles: () => ApiResult<OperatorRole[]> = async () => {
     try {
       const { data, pending, error, status } = await fetch<OperatorRole[]>(
-        '/api/v1/merchants2/operator/roles'
+        '/api/v1/merchants2/operators/roles'
       );
 
       isLoading.value = pending.value;
